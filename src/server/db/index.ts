be676669fe495cec26db9e9f4e5aacdcd7ec4b32
Jeppe Wolff Johansen <Jepper123411@proton.me>
2025-03-30 18:11:01 +0200

    Fixed linting

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,28 +1,28 @@
 import { drizzle } from "drizzle-orm/singlestore";
-import { createPool, type Pool } from "mysql2/promise";
+import { type Pool, createPool } from "mysql2/promise";
 
 import { env } from "~/env";
 
 import * as schema from "./schema";
 
 const globalForDb = globalThis as unknown as {
-  conn: Pool | undefined;
+	conn: Pool | undefined;
 };
 
 const conn =
-  globalForDb.conn ??
-  createPool({
-    host: env.SINGLESTORE_HOST,
-    port: Number.parseInt(env.SINGLESTORE_PORT),
-    user: env.SINGLESTORE_USER,
-    password: env.SINGLESTORE_PASS,
-    database: env.SINGLESTORE_DB_NAME,
-    ssl: {},
-    maxIdle: 0,
-  });
+	globalForDb.conn ??
+	createPool({
+		host: env.SINGLESTORE_HOST,
+		port: Number.parseInt(env.SINGLESTORE_PORT),
+		user: env.SINGLESTORE_USER,
+		password: env.SINGLESTORE_PASS,
+		database: env.SINGLESTORE_DB_NAME,
+		ssl: {},
+		maxIdle: 0,
+	});
 
 if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 conn.addListener("error", (err) => {
-  console.error("Database connection error:", err);
+	console.error("Database connection error:", err);
 });
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,12 @@
-import { int, bigint, text, singlestoreTable } from "drizzle-orm/singlestore-core";
+import {
+	bigint,
+	int,
+	singlestoreTable,
+	text,
+} from "drizzle-orm/singlestore-core";
 
 export const users = singlestoreTable("users_table", {
-  id: bigint("id", { mode: "bigint" }).primaryKey().autoincrement(),
-  name: text("name"),
-  age: int("age"),
+	id: bigint("id", { mode: "bigint" }).primaryKey().autoincrement(),
+	name: text("name"),
+	age: int("age"),
 });
