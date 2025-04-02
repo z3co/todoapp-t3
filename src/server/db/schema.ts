2a85f14c9c0c3b4515c9427493094f644d770b17
Jeppe Wolff Johansen <jepper123411@proton.me>
2025-04-02 10:16:54 +0200

    Created new db schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,12 +1,30 @@
 import {
 	bigint,
-	int,
-	singlestoreTable,
+	boolean,
+	index,
+	singlestoreEnum,
+	singlestoreTableCreator,
 	text,
+	timestamp,
 } from "drizzle-orm/singlestore-core";
 
-export const users = singlestoreTable("users_table", {
-	id: bigint("id", { mode: "bigint" }).primaryKey().autoincrement(),
-	name: text("name"),
-	age: int("age"),
-});
+export const createTable = singlestoreTableCreator(
+	(name) => `todoapp-t3_${name}`,
+);
+
+export const todo_table = createTable(
+	"todo_table",
+	{
+		id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
+		title: text("name").notNull(),
+		completed: boolean("age").notNull(),
+		priority: singlestoreEnum(["low", "medium", "high"]).notNull(),
+		ownerId: text("owner_id").notNull(),
+		createdAt: timestamp("created_at").notNull().defaultNow(),
+	},
+	(t) => {
+		return [index("owner_id_index").on(t.ownerId)];
+	},
+);
+
+export type DB_TodoType = typeof todo_table.$inferSelect;
