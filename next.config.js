fe106a13bc75abb1e6adbe71df6455a961ef22c7
Jeppe Wolff Johansen <Jepper123411@proton.me>
2025-04-08 20:44:40 +0200

    Removed revwrites from nextconfig as we are deploying to netlify

--- next.config.js
+++ next.config.js
@@ -6,25 +6,6 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-  async rewrites() {
-    return [
-      {
-        source: "/ingest/static/:path*",
-        destination: "https://us-assets.i.posthog.com/static/:path*",
-      },
-      {
-        source: "/ingest/:path*",
-        destination: "https://us.i.posthog.com/:path*",
-      },
-      {
-        source: "/ingest/decide",
-        destination: "https://us.i.posthog.com/decide",
-      },
-    ];
-  },
-  // This is required to support PostHog trailing slash API requests
-  skipTrailingSlashRedirect: true,
-
   eslint: {
     ignoreDuringBuilds: true,
   },
