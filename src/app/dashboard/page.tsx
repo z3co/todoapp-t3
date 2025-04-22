8ce9b96aa859de37875750161823ccc0b944f1b4
z3co <143700752+z3co@users.noreply.github.com>
2025-04-22 12:11:42 +0200

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,11 +6,7 @@ import { QUERIES } from "~/server/db/queries";
 export default async function DashboardPage() {
   const user = await auth();
   if (!user.userId) return redirect("/");
-  try {
   const todos = await QUERIES.getTodos(user.userId);
-  } catch(error) {
-    console.log("Error getting todos:", error);
-  }
   return (
     <main className="min-h-screen bg-gray-50">
       <TodoDashboard initialTodos={todos} />
